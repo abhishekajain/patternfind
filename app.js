a8414a108fb058f715d96209fbabe0363e8fb210
Abhishek Jain <abhishek.ajain@gmail.com>
2016-08-01 12:12:39 -0700

    commit: 	modified:   app.js

--- app.js
+++ app.js
@@ -5,16 +5,12 @@ const fs = require('fs');
 readLinesLbL();
 
 function findMatchingArray(arg1, myArray){
-	//console.log(arg1);
-	//console.log(myArray);
 	var resultArr = [];	
 	if(searchedPatternArr.indexOf(arg1) <= -1){
-		//console.log(searchedPatternArr+':'+searchedPatternArr.indexOf(arg1)+':'+arg1);
 		var myObj;
 		searchedPatternArr.push(arg1);
 		for (myObj in myArray) {
 			var arrayObject = myArray[myObj];
-			//console.log(arrayObject);
 			if(arrayObject.indexOf(arg1) !== -1){
 				resultArr.push(arrayObject);
 			}
@@ -23,44 +19,37 @@ function findMatchingArray(arg1, myArray){
 	return resultArr;	
 }
 
-function arrayfind(thisarg){
-	//console.log(element+index+ array+ thisarg);
-	var key;
-	if(key in searchedPatternArr){
-		if(searchedPatternArr[key] === thisarg) {
-			return true;
-		}
-	}
-	return false;
-}
+//array variable to hold patterns that found mataching sequence.
 var searchedPatternArr = [];
-
+/**
+Function that stat write stream of result.txt file.
+Loop all the data collected from reading file.
+Calls patternBreaker function that will further break each sequence.
+*/
 function findPattern(inputArray){
 	var writeStream = fs.createWriteStream('./result.txt');
 	var key;
 	for(key in inputArray){
-		//console.log(key);
 		var value = inputArray[key];
 		var valueLan = value.length;
-		//console.log(value);
 		var i;
 		for (i = 0; i < valueLan; i++) {
 			var newVal = value.substring(i, valueLan);
-			//console.log(newVal);
 			patternBreaker(newVal, writeStream, inputArray);
 		}		
 	}
 	writeStream.end();
 }
-
+/**
+Function break sequence in reverse and calls findMatchingArray seqeuence of pattern.
+Write results in the file.
+*/
 function patternBreaker(value, writeStream, inputArray){
 	var valueLan = value.length;
 	var i;
 	for (i = valueLan; i > 2; i--) { 
 		var newVal = value.substring(0, i);
 		var retArray = findMatchingArray(newVal, inputArray);
-		//console.log(newVal);
-		//console.log(retArray);
 		if(retArray.length>1){
 			writeStream.write('results of pattern :'+newVal+':');
 			writeStream.write('['+retArray+']\n');
@@ -68,7 +57,10 @@ function patternBreaker(value, writeStream, inputArray){
 	}	
 }
 
-
+/**
+This function read data file and create Collection of data.
+Once data collection is complete it calls function that will start finding patterns in each element of collection.
+*/
 function readLinesLbL(){
 	var lr = new LineByLineReader('data.txt');
 	var resultArr = [];
@@ -77,7 +69,6 @@ function readLinesLbL(){
 	});
 
 	lr.on('end', function () {
-		//console.log(resultArr);
 		findPattern(resultArr);
 	});	
 }
